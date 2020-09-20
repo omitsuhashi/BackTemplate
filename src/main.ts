51b32d1cf38bcad7463b73e911797c4eebc70f52
Osamu Mitsuhashi <osamu.mitsuhashi224@gmail.com>
2020-09-20 16:21:16 +0900

    add: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const documentOptions = new DocumentBuilder()
+    .setTitle('des2a')
+    .setDescription('desire to')
+    .setVersion('1.0')
+    .addTag('beta')
+    .build();
+  const document = SwaggerModule.createDocument(app, documentOptions);
+  SwaggerModule.setup('swagger', app, document);
   await app.listen(3000);
 }
 bootstrap();
