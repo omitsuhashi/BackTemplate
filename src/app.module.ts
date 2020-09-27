410a1caa7c3df79b61c31eb69575c04897a8f99b
Osamu Mitsuhashi <osamu.mitsuhashi224@gmail.com>
2020-09-27 19:33:15 +0900

    add: typeorm and db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const databaseConfig = TypeOrmModule.forRoot({
+  type: 'mysql',
+  host: 'localhost',
+  port: 8050,
+  username: 'test',
+  password: 'test',
+  database: 'des2a',
+  synchronize: true,
+})
+
 @Module({
-  imports: [],
+  imports: [databaseConfig],
   controllers: [AppController],
   providers: [AppService],
 })
